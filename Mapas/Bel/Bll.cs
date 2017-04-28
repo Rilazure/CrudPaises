41f45dbeb07e0b72acebcde434d77dbe2ef569ff
Ricardo Lara <Ricardo.lara06@hotmail.com>
2017-04-28 16:38:41 +0000

    Creacion Capa Negocios

---
+++ Mapas/Bel/Bll.cs
@@ -0,0 +1,32 @@
+﻿using Dal;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bel
+{
+    public class Bll
+    {
+        Data Dl = new Data();
+
+        public DataTable ConsultarUsuariosB()
+        {
+            return Dl.ConsultarUsuarios();
+        }
+        public void InsertUsuarioB(string Nombre,int Fk_Ciudad)
+        {
+            Dl.insertarUsuario(Nombre,Fk_Ciudad);
+        }
+        public void ActualizarB(int IdUsuario)
+        {
+            Dl.Actualizar(IdUsuario);
+        }
+        public void EliminarB(int IdUsuario)
+        {
+            Dl.Eliminar(IdUsuario);
+        }
+    }
+}
