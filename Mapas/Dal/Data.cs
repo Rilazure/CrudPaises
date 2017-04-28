fafa2a8600e58426d0da54f8c226e04bd1e0695d
Ricardo Lara <Ricardo.lara06@hotmail.com>
2017-04-28 14:44:59 +0000

    Se crea el repositorio y se crea conezion a bd

---
+++ Mapas/Dal/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    public class Data
+    {
+        private SqlConnection cx;
+        private void Conexion()
+        {
+            string cadena = ConfigurationManager.ConnectionStrings["dbz"].ConnectionString;
+        }
+        public void ConsultarUsuarios()
+        {
+
+        }
+    }
+}
